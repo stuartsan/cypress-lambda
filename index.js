4f6e4f10816798ed4b9fafe263ca195fa973ba80
Stuart Sandine <stuajc@gmail.com>
2019-04-08 15:55:18 -0700

    prettiar

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const glob = require('glob');
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const glob = require("glob");
 
-const lambda = new AWS.Lambda({ region: 'us-west-2' });
+const lambda = new AWS.Lambda({ region: "us-west-2" });
 
-const lambdaArn = fs.readFileSync('./deployed_lambda_arn').toString();
-
+const lambdaArn = fs.readFileSync("./deployed_lambda_arn").toString();
 
 async function main() {
-  const files = glob.sync('cypress/integration/**/*.spec.js', {
-    cwd: 'lambda',
-  }).map(file => `/tmp/${file}`);
+  const files = glob
+    .sync("cypress/integration/**/*.spec.js", {
+      cwd: "lambda"
+    })
+    .map(file => `/tmp/${file}`);
 
   try {
-    const results = await Promise.all(files.map(file => {
-      return lambda.invoke({
-          FunctionName: lambdaArn,
-          Payload: JSON.stringify({ cypressSpec: file })
-      }).promise();
-    }));
+    const results = await Promise.all(
+      files.map(file => {
+        return lambda
+          .invoke({
+            FunctionName: lambdaArn,
+            Payload: JSON.stringify({ cypressSpec: file })
+          })
+          .promise();
+      })
+    );
 
     results.forEach((result, idx) => {
-      fs.writeFileSync(`reports/mochawesome-${idx}.json`,  JSON.parse(result.Payload))
+      fs.writeFileSync(
+        `reports/mochawesome-${idx}.json`,
+        JSON.parse(result.Payload)
+      );
     });
-
   } catch (e) {
     console.error(e);
   }
